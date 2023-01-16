5d89addede3bb3420cae043f49d9c55051130790
shubhampandey7860 <you@example.com>
2023-01-17 00:16:32 +0530

    slider menu

--- Gui project/Slider menu.py
+++ Gui project/Slider menu.py
@@ -5,6 +5,17 @@ App = Tk()
 App.title('slider menu')
 slid_value = IntVar()
 App.geometry('300x300')
-sli = Scale(App, from_=0, to=100)
+sli = Scale(App, from_=0, to=100,variable=slid_value,orient=HORIZONTAL)
+sli.pack()
+
+
+#
+def show_slider():
+    msg = Label(App, text=slid_value.get(),font=('arial',10))
+    msg.pack()
+
+
+B = Button(App, text='show', command=show_slider,background='red',foreground='white',font=('arial',10))
+B.pack()
 
 App.mainloop()
